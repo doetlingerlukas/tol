2161a32b6745758c799454b4b1fb3a09925b4f46
Lukas Dötlinger <lukas.doetlinger@student.uibk.ac.at>
2021-01-15 15:55:34 +0100

    Render sprite.

--- main.cpp
+++ main.cpp
@@ -1,17 +1,26 @@
+#include <iostream>
+
 #include <SFML/Graphics.hpp>
 
 int main() {
 
   sf::RenderWindow window(sf::VideoMode(840, 600), "SFML works!", sf::Style::Titlebar | sf::Style::Close);
 
-  sf::CircleShape shape(100.f);
-  shape.setFillColor(sf::Color::Green);
+  // https://opengameart.org/node/8122
+  sf::Texture floor_texture;
+  floor_texture.setSmooth(true);
+  if (!floor_texture.loadFromFile("textures/stonetiles.png")) {
+    std::cerr << "Failed to load texture!" << std::endl;
+  }
 
-  while(window.isOpen()) {
+  sf::Sprite floor;
+  floor.setTexture(floor_texture);
+
+  while (window.isOpen()) {
 
     sf::Event event;
     
-    while(window.pollEvent(event)) {
+    while (window.pollEvent(event)) {
       switch (event.type) {
       case sf::Event::Closed:
         window.close();
@@ -24,8 +33,8 @@ int main() {
       }
     }
 
-    window.clear();
-    window.draw(shape);
+    window.clear(sf::Color::Black);
+    window.draw(floor);
     window.display();
   }
 
