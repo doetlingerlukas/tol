e18e0aa6c785a73ec6883af9d1df5902936ee399
Markus Reiter <me@reitermark.us>
2021-01-31 03:56:40 +0100

    Fix nuklear menu.

--- include/nuklear.hpp
+++ include/nuklear.hpp
@@ -7,8 +7,6 @@
 #define NK_INCLUDE_VERTEX_BUFFER_OUTPUT
 #define NK_INCLUDE_FONT_BAKING
 #define NK_INCLUDE_DEFAULT_FONT
-#define NK_IMPLEMENTATION
-#define NK_SFML_GL2_IMPLEMENTATION
 
 #include "nuklear.h"
 #include "nuklear_sfml_gl2.h"
