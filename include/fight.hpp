9adc6a326ea0c7b9bf7f228eb56d5354337f01c8
Michael Kaltschmid <kaltschmidmichael@gmail.com>
2021-02-15 04:56:17 +0100

    Draw health bar of player in fight.

--- include/fight.hpp
+++ include/fight.hpp
@@ -33,12 +33,42 @@ class Fight: public sf::Drawable, public sf::Transformable {
 
     const int resize_x = Fight::scale_ultra(target.getSize().x, target.getSize().y);
 
-    sf::Sprite player;
-    player.setTexture(*asset_cache->loadTexture("tilesets/character-whitebeard.png"));
-    player.setTextureRect(sf::IntRect{ TILE_SIZE, TILE_SIZE * 3, TILE_SIZE, TILE_SIZE });
-    player.setPosition({ target.getSize().x - resize_x - 100.0f, target.getSize().y - (100.0f * scale_factor) });
-    player.setScale({ scale_factor, scale_factor });
-    target.draw(player);
+    sf::Sprite player_sprite;
+    player_sprite.setTexture(*asset_cache->loadTexture("tilesets/character-whitebeard.png"));
+    player_sprite.setTextureRect(sf::IntRect{ TILE_SIZE, TILE_SIZE * 3, TILE_SIZE, TILE_SIZE });
+    player_sprite.setPosition({ target.getSize().x - resize_x - 100.0f, target.getSize().y - (100.0f * scale_factor) });
+    player_sprite.setScale({ scale_factor, scale_factor });
+    target.draw(player_sprite);
+
+    auto health = player.getStats()->health().get();
+
+    sf::RectangleShape health_background;
+    health_background.setSize({ 440.0f, 30.0f });
+    health_background.setPosition({ static_cast<float>(target.getSize().x - resize_x), target.getSize().y - 150.0f });
+    health_background.setFillColor(sf::Color(255, 232, 225, 100));
+    target.draw(health_background);
+
+    sf::RectangleShape health_fill;
+    health_fill.setSize({ 440.0f * (health / 100.0f), 30.0f });
+    health_fill.setPosition({ static_cast<float>(target.getSize().x - resize_x), target.getSize().y - 150.0f });
+
+    if (health > 40 && health < 70) {
+      health_fill.setFillColor(sf::Color(255, 165, 0, 255));
+    } else if (health >= 70) {
+      health_fill.setFillColor(sf::Color(36, 109, 36, 255));
+    } else {
+      health_fill.setFillColor(sf::Color(255, 0, 0, 255));
+    }
+
+    target.draw(health_fill);
+
+    sf::Text player_health_percent;
+    player_health_percent.setCharacterSize(22);
+    player_health_percent.setFillColor(sf::Color::White);
+    player_health_percent.setString(fmt::format("{}", health));
+    player_health_percent.setFont(*asset_cache->loadFont("fonts/Gaegu-Bold.ttf"));
+    player_health_percent.setPosition({ target.getSize().x - resize_x + 200.0f, target.getSize().y - 152.0f });
+    target.draw(player_health_percent);
 
     sf::Sprite enemy;
 
