596c406fcea2c3366a6078f7f204c55c77307099
Lukas Dötlinger <lukas.doetlinger@student.uibk.ac.at>
2021-02-18 00:02:10 +0100

    Fixes after rebase.

--- include/game.hpp
+++ include/game.hpp
@@ -67,7 +67,7 @@ class Game {
           mouse_pressed = event.type == sf::Event::MouseButtonPressed;
 
           if (event.mouseButton.button == sf::Mouse::Button::Left) {
-            inventory_overlay.mouse(
+            inventory.mouse(
               { static_cast<float>(event.mouseButton.x), static_cast<float>(event.mouseButton.y) }, mouse_pressed);
           }
         } else {
