6a6ab60a65acc66958a662e2773883397a72a1d1
Markus Reiter <me@reitermark.us>
2021-02-19 19:50:30 +0100

    Fix `deps.sh`.

--- deps.sh
+++ deps.sh
@@ -15,14 +15,16 @@ if [[ "${uname}" == 'Darwin' ]]; then
 elif [[ "${uname}" == 'Linux' ]]; then
   sudo apt-get update
   sudo apt-get install -y build-essential tar curl cmake zip unzip gcc ruby ninja-build \
-    libx11-dev libxrandr-dev libxi-dev libudev-dev libgl1-mesa-dev \
+    libx11-dev libxrandr-dev libxi-dev libudev-dev libgl1-mesa-dev pkg-config \
     clang-format clang-tidy
 
-  git clone https://github.com/microsoft/vcpkg
-  pushd vcpkg
-  ./bootstrap-vcpkg.sh
-  export PATH="$(pwd):${PATH}"
-  popd
+  if ! [[ -d vcpkg ]]; then
+    git clone https://github.com/microsoft/vcpkg
+    pushd vcpkg
+    ./bootstrap-vcpkg.sh
+    export PATH="$(pwd):${PATH}"
+    popd
+  fi
 
   if ! which tiled; then
     sudo curl -L https://github.com/mapeditor/tiled/releases/download/v1.4.3/Tiled-1.4.3-x86_64.AppImage -o /usr/local/bin/tiled
